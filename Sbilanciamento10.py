51e4bfd1626056a9169e9ec27936c6f4baabe0e1
davideflo <davideflo@users.noreply.github.com>
2017-12-11 09:19:23 +0100

    Bugs fixing

--- Sbilanciamento10.py
+++ Sbilanciamento10.py
@@ -743,6 +743,7 @@ def PredictOOS_Base(pdo, sos, rical, db, zona, dts):
             counter += 1
     
     PRED = pd.DataFrame.from_dict(PRED, orient = 'index')
+    PRED = PRED[PRED.columns[:26]]
     PRED.columns = ['POD', 'Giorno', '1','2','3', '4', '5', '6', '7', '8', '9', '10',
                 '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24']    
     Pred = PRED.fillna(0)
